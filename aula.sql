4f52af3ca39cbd5fd637addb647b91c9cdd7ff72
daianasantosferreira <125597466+daianasantosferreira@users.noreply.github.com>
2024-03-26 16:02:37 -0300

    Create aula.sql

---
+++ aula.sql
@@ -0,0 +1,142 @@
+CREATE TABLE Alunos (
+    ID int,
+    Nome varchar(255),
+    Sobrenome varchar(255),
+    Idade varchar(255)
+   
+);
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Daiana' ,'Ferreira' , '18',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Victor' ,'Hugo' , '16',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Annely' ,'Carrero' , '16',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Gabriella' ,'Boarati' , '16',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Heloisa' ,'Oliveira' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Milleny' ,'Leopoldino' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Isabella' ,'Patricio' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Victor' ,'Hugo' , '16',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Vinicius' ,'Pavani' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Evelyn' ,'Moura' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Emmily' ,'OLiveira' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Gustavo' ,'Argetino' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Gabriel' ,'Moreira' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'João' ,'Marcondes' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'João' ,'Marcondes' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Enzo' ,'Santos' , '16',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Ryan' ,'Brandão' , '18',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Isabella' ,'Bertolo' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Geovana' ,'Ferreira' , '16',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Geovana' ,'Nascimento' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Caroline' ,'Camargo' , '16',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Isadora' ,'Silveira' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Livia' ,'Macedo' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Victor' ,'Calori' , '16',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Thiago' ,'Lima' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Isabella' ,'Almeida' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Pietra' ,'Basso' , '16',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Andrey' ,'Souza' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Pedro' ,'Pizarro' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Hugo' ,'Romoda' , '18',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Wesley' ,'Batista' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Adrielly' ,'Inocencio' , '17',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Oliver' ,'Osorio' , '18',);
+
+INSERT INTO Alunos
+(ID,Nome,Sobrenome,Idade,)
+VALUES (1, 'Alice' ,'Campos' , '17',);
